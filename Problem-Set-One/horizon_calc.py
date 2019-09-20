8f5d74934f6eb49c2b0391267e7b67277f8898e0
Nayalash Mohammad <1nayalashm@gmail.com>
2019-09-20 18:45:33 -0400

    added horizon_calc

---
+++ Problem-Set-One/horizon_calc.py
@@ -0,0 +1,27 @@
+# Author: Nayalash Mohammad
+# Date: September 20 2019
+# File Name: horizon_calc.py
+# Description: A program that calculates your distance to the horizon
+
+import math
+
+def calculateDistanceToHorizon():
+
+    # Get distance and time inputs from user
+    height = float(input("What is the height from the ground (in centimeters)"))
+
+    try:
+        # Use Distance Formula for the baseball (D = sqrt(0.126 * H))
+        calculation = (math.sqrt(0.126 * height)) / 10000
+
+
+        print("The distance to horizon is : " + str(calculation) + " km")
+
+        # If there is an error (invalid value), print message to user.
+    except ValueError:
+        print("Sorry Invalid Number")
+
+
+#Invoke Method
+calculateDistanceToHorizon()
+
