c4821381abeb06aa110792ee57eaa533437c351c
Nayalash Mohammad <1nayalashm@gmail.com>
2019-09-21 18:36:00 -0400

    added digitbreaker program

--- Problem-Set-One/digit_breaker.py
+++ Problem-Set-One/digit_breaker.py
@@ -2,3 +2,11 @@
 # Date: September 20 2019
 # File Name: digit_breaker.py
 # Description: A program that splits the digits in a number
+
+# Ask a number from the user
+number = input("Enter a Three Digit Number: ")
+
+# Iterate over the provided string
+for i in number:
+    print(i)
+
