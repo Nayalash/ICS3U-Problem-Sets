0590771ea0445bb5dd92b04042de4a8b8bf14acc
Nayalash Mohammad <1nayalashm@gmail.com>
2019-09-23 16:31:58 -0400

    added problem set two

---
+++ Problem-Set-Two/vertical_stars.py
@@ -0,0 +1,11 @@
+# Author: Nayalash Mohammad
+# Date: September 23 2019
+# File Name: vertical_stars.py
+# Description: A program that asks a user for a number, and prints that many '*' characters in a vertical line.
+
+# Ask user for a number
+number = int(input("Enter a number: "))
+
+
+for i in range(number):
+    print("*")
