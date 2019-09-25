47d2c695c2907d9d2488fda66b8e9ca989ccb627
Nayalash Mohammad <1nayalashm@gmail.com>
2019-09-25 18:54:08 -0400

    added PS2: 5

--- Problem-Set-Two/mohammad_password_checker.py
+++ Problem-Set-Two/mohammad_password_checker.py
@@ -4,15 +4,19 @@
 # Description: A program that asks you for a master password,
 # and allows you to keep trying the password until you get it right.
 
+# Ask user for the password they are trying to guess
 masterPassword = input("Enter Your Master Password: ")
 
+# Set passeword equal to None (NULL) for place holder reasons
 password = None
 
+# Check if user entered the right password
 while(masterPassword != password):
     password = input("Enter Password: ")
 
+    # When user enters correct password, print something
     while(masterPassword == password):
         print("You Have Guessed The Correct The Password")
-        break
+        break # Use break, so message does not print repeatedly
 
 
