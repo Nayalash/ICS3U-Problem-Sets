f54acf826128724a5a743e79e111f086971fc5a4
Nayalash Mohammad <1nayalashm@gmail.com>
2019-09-26 20:24:57 -0400

    added last problem set for #3

---
+++ Problem-Set-Two/mohammad_roots_squares_cubes.py
@@ -0,0 +1,37 @@
+# Author: Nayalash Mohammad
+# Date: September 26 2019
+# File Name: sqaures_roots_cubes.py
+# Description: A program that asks a user for a range
+# and prints the square, root and cube of all the numbers
+#in that range.
+
+import math # Import Math Library, to use square root function
+
+# If user inputs not a integer, print something to console
+try:
+    # Ask user for a start and a stop
+    start = int(input("Start: "))
+    stop = int(input("Stop: "))
+
+    print(">>>>>>>>>>>>>>>>>>>")
+    print(">>>>>>>>>>>>>>>>>>>")
+
+except ValueError:
+    print("Invalid Value")
+
+# Initialize For Loop, with a start and stop
+for i in range(start, stop):
+
+    # Define Formulas for finding squares, roots and cubes
+    square = i * i
+    root = math.sqrt(i)
+    cube = i * i * i
+
+    # Print Results to Console
+    print("Number: " + str(i))
+    print("Square: " + str(square))
+    print("Root: " + str(root))
+    print("Cube: " + str(cube))
+    print("*******************")
+
+
