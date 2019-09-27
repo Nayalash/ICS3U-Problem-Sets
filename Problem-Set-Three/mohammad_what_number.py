add1aa5127a7c7851ea59396005dfa42924c4459
Nayalash Mohammad <1nayalashm@gmail.com>
2019-09-27 18:35:54 -0400

    added PS3: 1

---
+++ Problem-Set-Three/mohammad_what_number.py
@@ -0,0 +1,24 @@
+# Author: Nayalash Mohammad
+# Date: September 27 2019
+# File Name: what_number.py
+# Description: A program that asks a user for a number,
+# tells whether it is odd or even, or positive/negative/zero
+
+try:
+    number = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid Input (ONLY INTEGER TYPE IS ALLOWED")
+
+if ((number % 2) == 0):
+    print("This number is even...")
+else:
+    print("This number is odd...")
+
+if number < 0:
+    print("This number is also negative...")
+elif number == 0:
+    print("This number is also zero...")
+else:
+    print("This number is also positive...")
+
+
--- Problem-Set_Three/mohammad_what_number.py
+++
@@ -1,1 +0,0 @@
-# Name: Nayalash Mohammad
