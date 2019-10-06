51cd833fbd466a9af4766518faf73a4d368ce9e5
Nayalash Mohammad <1nayalashm@gmail.com>
2019-10-06 14:49:08 -0400

    added PS3: 5

--- Problem-Set-Three/mohammad_postage.py
+++ Problem-Set-Three/mohammad_postage.py
@@ -4,26 +4,55 @@
 # Description: A program that tells the user the price of postage
 # for a package based on weight.
 
-# Ask the user for the weight of their package
-weight = int(input("What is the weight of your letter in grams: "))
+# DOCUMENTATION ON README FILE, CALLED "postage_README.txt"
 
-# Start out the cost variable to be zero
-cost = 0
+print("Welcome to Postage Price Calculator\nTo run the app, press r, to quit the app press q, for help press h \n")
 
-# Implement the test cases
-if (weight < 30):
-    cost = 48
+def mainComputation():
+    # Ask the user for the weight of their package
+    weight = int(input("What is the weight of your letter in grams: "))
 
-elif (31 < weight < 50):
-    cost = 70
+    # Start out the cost variable to be zero
+    cost = 0
 
-elif (51 < weight <= 100):
-    cost = 90
+    # Implement the test cases
+    if (weight <= 30):
+        cost = 48
 
-elif (weight > 100):
-    # For Every 50 grams add 18 cents
-    weight -= 100
-    cost = 90 + (18 * (weight // 50))
+    elif (31 < weight < 50):
+        cost = 70
 
-# Display final price to user
-print("Price of postage is: " + str(cost) + " cents.")
+    elif (51 < weight <= 100):
+        cost = 90
+
+    elif (weight > 100):
+        # For Every 50 grams add 18 cents
+        weight -= 100
+        cost = 90 + (18 * (weight // 50))
+
+    # Display final price to user
+    print("Price of postage is: " + str(cost) + " cents. \nTo run again press `r`, to quit press `q`, for help press `h`")
+
+def menu():
+    run = input()
+
+    # Menu Options
+    if (run == "r") :
+        mainComputation()
+        menu()
+
+    elif (run == 'q'):
+        exit()
+        menu()
+
+    elif (run == 'h'):
+        print("To Learn More About This Application. Visit the mohammad_postage_README.txt file.")
+        print("To run the app, press r, to quit the app press q, for help press h")
+        menu()
+
+    else:
+        print("Invalid Input, try running command `h`, for more details")
+        menu()
+
+# Initiate Program
+menu()
---
+++ Problem-Set-Three/mohammad_postage_README.txt
@@ -0,0 +1,40 @@
+Author: Nayalash Mohammad
+
+Class: ICS3U1 - Period 2 - Berardi, A
+
+Date: October 7 2019
+
+Version: 1.0.0
+
+Unit 2: Problem Set Three, Question 5
+
+Programming Language: Python 3.7.6
+
+Description: A program that tells the user the price of postage for a package based on weight.
+Running File Name: mohammad_postage.py
+
+Program Assumptions: In order to run this program, you need a computer running either MacOS, Windows or Linux, with Python 3 or greater,
+having the Python Environmental Variable set to your PATH
+
+Features:
+
+    • A always on menu, where the menu always shows up after each computation is completed.
+    • Exact Calculation Of Price, along with helpful reminders of how to quit the program after use.
+
+Restrictions:
+
+    • This Program Contains Absolutely NO Restrictions
+
+Known Errors:
+
+    • There are NO "Known Errors", for this program after testing
+
+Implementation Details:
+
+    • Used the Postage Algorithm, provided by problem set three.
+    • To have the continuously running menu, I applied the methodology of recursive functions.
+
+Additional Files:
+
+    • mohammad_postage.py
+    • mohammad_postage_README.txt
