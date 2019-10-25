03f0dc9ce1b228fccc78f1aa9374322361c286a3
Mohammad Nayalash <38357017+Nayalash@users.noreply.github.com>
2019-10-25 10:37:28 -0400

    Update mohammad_island_dice.py

--- Problem-Set-Six/mohammad_island_dice.py
+++ Problem-Set-Six/mohammad_island_dice.py
@@ -3,28 +3,72 @@
 # File Name: island_dice.py
 # Description: A program that follows the island dice algorithm.
 
+
+# Use random library to use randomint function
 import random
 
+# Starting number of shells
 shells = 1000
 
-while True:
-    riskAmount = int(input("How many shells would you like to risk?: "))
-    if riskAmount > shells:
-        print("You don't have enough shells!")
-        continue
-    bet = input("Do you predict a high or low number?: ")
-    betIsHigh = False
-    if(bet.lower() == "high"):
-        betIsHigh = True
+def main(shells):
 
-    shells -= riskAmount
-    result = random.randint(1, 12)
+    while True:
+        # Ask how many shells to risk
+        riskAmount = int(input("How many shells would you like to risk?: "))
 
-    if(result == 7):
-        print("Rolled a total of 7. Better luck next time, you lost :(")
-    elif(betIsHigh and result < 7 or not betIsHigh and result > 7):
-        print("You rolled a total of " + str(result) + " which isn't in the range you predicted. Sorry, better luck next time.")
+        # Check if user is not going over their total number of shells
+        if riskAmount > shells:
+            print("You don't have enough shells!")
+            continue
+        # Ask them for high or low prediction
+        bet = input("Do you predict a high or low number?: ")
+        
+        betIsHigh = False
+        
+        if(bet.lower() == "high"):
+            betIsHigh = True
+
+        # Subtract by risked shells
+        shells = shells - riskAmount
+        # Combine two dice into one
+        result = random.randint(1, 12)
+
+        # Use Win-Lose Algorithm Provided
+        if(result == 7):
+            print("Rolled a total of 7. Better luck next time, you lost :(")
+        elif(betIsHigh and result < 7 or not betIsHigh and result > 7):
+            print("You rolled a total of " + str(result) + " which isn't in the range you predicted. Sorry, better luck next time.")
+        else:
+            # Double the shells
+            shells = shells + ((riskAmount * 2))
+            print("You rolled a total of " + str(result) + " which is in the range you predicted. Congrats.")
+
+        # Print Shell Total
+        print("Your shell total is: " + str(shells))
+
+        # Prompt user for menu
+        menu()
+
+
+# Menu Method
+def menu():
+    print("To Run Program: type run, To Quit: type quit, For Help: type help")
+    # Ask user for what they would like to do
+    option = input()
+
+    if (option == "run"):
+        main(shells) # Run App
+        menu()
+    elif (option == "quit"):
+        exit()
+    elif (option == "help"):
+        print('To run program, type `run` below, or `quit` to quit the program. Refer to docs for more info.')
+        print('Type how many shells you want to risk, then enter if you predict a high or low number')
+        menu()
     else:
-        shells += riskAmount * 2
-        print("You rolled a total of " + str(result) + " which is in the range you predicted. Congrats.")
-    print("Your shell total is: " + str(shells))
+        print("Invalid Input")
+        menu()
+
+# Init Program
+print("Welcome To The Island Dice Game")
+menu()
